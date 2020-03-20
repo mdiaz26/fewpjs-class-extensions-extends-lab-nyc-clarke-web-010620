3ff1e8a0485b75fd62f384a9a50cca8a25caf927
mdiaz26 <mike.diaz006@gmail.com>
2020-03-20 09:43:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+class Polygon {
+    constructor(arrayOfIntegers) {
+        this.sideLengths = arrayOfIntegers
+    }
+
+    get countSides(){
+        return this.sideLengths.length
+    }
+
+    get perimeter(){
+        return this.sideLengths.reduce(function(accumulator, currentValue){return accumulator + currentValue}, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        if ((this.sideLengths[0] + this.sideLengths[1] > this.sideLengths [2]) &&
+        (this.sideLengths[1] + this.sideLengths[2] > this.sideLengths[0]) &&
+        (this.sideLengths[0] + this.sideLengths[2] > this.sideLengths[1])){
+            return true
+        } else {return false}
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        let average = (this.sideLengths.reduce(function(accumulator, currentValue){return accumulator + currentValue}, 0))/4
+        return (average === this.sideLengths[0])
+    }
+
+    get area(){
+        return this.sideLengths[0] ** 2
+    }
+}
